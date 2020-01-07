4464aa88630865e76f6c6b401b58a594dcc4688c
paul-xiao <box17@163.com>
2020-01-08 00:00:13 +0800

    'init'

---
+++ src/components/tree-item.vue
@@ -0,0 +1,44 @@
+
+<template>
+    <ul class="tree">
+      <li v-for="item in folders" :key="item.id">
+        {{item.name}}
+         <template>
+
+         </template>
+      </li>
+    </ul>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "TreeItem",
+  data() {
+    return {
+     
+    };
+  },
+  computed: {
+    ...mapGetters(["folders"])
+  },
+  created() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.$store.commit("INIT_FOLDER");
+      this.add(this.data[0]);
+    },
+    add(node) {
+      this.$store.commit("ADD_FOLDER", node);
+    }
+  }
+};
+</script>
+
+<style>
+.tree {
+  text-align: left;
+}
+</style>
---
+++ src/store/folder.js
@@ -0,0 +1,21 @@
+const folder = {
+    state: {
+        folders: []
+    },
+    mutations:{
+        INIT_FOLDER(state) {
+           state.folders = []
+        },
+        ADD_FOLDER(state, node) {
+           state.folders.push(node)
+        }
+    },
+    actions: {
+
+    },
+    getters: {
+     folders: state => state.folders
+    }
+}
+
+export default folder
